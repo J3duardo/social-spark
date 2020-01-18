3da938d9e1c3b68106d3d1bf70238fe732a35923
J3duardo <jesus_69eduardo@hotmail.com>
2020-01-18 14:14:19 -0400

    Client side: Barra de navegación principal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+import "./App.scss";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-        </Switch>
+        <NavBar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+          </Switch>
+        </div>
       </BrowserRouter>
     </div>
   );
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
+import {Link} from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
 
 class NavBar extends Component {
   render() {
     return (
-      <div>
-        Navbar
-      </div>
+      <AppBar position="fixed">
+        <Toolbar className="nav-container">
+          <Button color="inherit" component={Link} to="/">Home</Button>
+          <Button color="inherit" component={Link} to="/login">Login</Button>
+          <Button color="inherit" component={Link} to="/signup">Signup</Button>
+        </Toolbar>
+      </AppBar>
     );
   }
 }
