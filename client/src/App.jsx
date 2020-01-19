5ed6e48144fde1250bdf52bd6c5e66516061462d
J3duardo <jesus_69eduardo@hotmail.com>
2020-01-19 11:01:52 -0400

    Client side: Estilos globales

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,46 @@
 import React from "react";
 import "./App.scss";
+import {MuiThemeProvider} from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import NavBar from "./components/NavBar";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#33c9dc",
+      main: "#00bcd4",
+      dark: "#008394",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#ff6333",
+      main: "#ff3d00",
+      dark: "#b22a00",
+      contrastText: "#fff"
+    }
+  }
+})
+
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <NavBar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div>
+        <BrowserRouter>
+          <NavBar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
