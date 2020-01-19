01296e59f306dcb09f3da1a2de16eeba990b0239
J3duardo <jesus_69eduardo@hotmail.com>
2020-01-19 11:03:24 -0400

    Client side: Mostrar los posts de los usuarios en el home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,45 @@
 import React, { Component } from "react";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+import Post from "../components/Post";
 
 class Home extends Component {
+  state = {
+    posts: []
+  };
+
+  async componentDidMount() {
+    const response = await axios({
+      method: "GET",
+      url: "/posts"
+    });
+
+    this.setState({
+      posts: response.data.data
+    })
+  };
+
   render() {
+    console.log(this.state.posts)
+
+    const renderPosts = () => {
+      if(this.state.posts.length === 0) {
+        return <p>Loading..</p>
+      }
+      return this.state.posts.map((post) => {
+        return <Post key={post.id} post={post} />
+      })
+    }
+
     return (
-      <div>
-        Home
-      </div>
+      <Grid container>
+        <Grid item sm={8} xs={12}>
+          {renderPosts()}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile</p>
+        </Grid>
+      </Grid>
     );
   }
 }
